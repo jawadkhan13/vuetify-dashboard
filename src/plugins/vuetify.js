1e37ec08685d6dc752032bb02d71bca74788c3b6
Jawad Khane <jawadkhan.cse@gmail.com>
2022-07-04 21:54:49 +0500

    Final commit

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,17 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'mdi'
+      },
+      theme: {
+        themes: {
+          light: {
+            primary: '#41B883'
+          },
+          dark: {
+            primary: '#34495E'
+          }
+        }
+      }
 });
